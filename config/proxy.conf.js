8ad6803e21629cde54bcd2218eff73679015f5e2
yarah123 <yara.abiraad@kjtech.ai>
2025-03-23 01:56:57 +0300

    build custom-booking-ui

--- config/proxy.conf.js
+++ config/proxy.conf.js
@@ -1,5 +1,5 @@
 /** FQDN to proxy requests. i.e. No protocol and path should be in the value */
-const domain = 'placeos-dev.aca.im';
+const domain = 'smartoffice.dubaifuture.ae';
 /** Whether the proxied endpoints use SSL */
 const secure = true;
 /** Whether the SSL certificate used is valid on the internet */
